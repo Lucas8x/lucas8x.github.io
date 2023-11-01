3e6901d10192daaa25b86c8d96339162977e704a
Lucas8x <lucasemanuel8x@gmail.com>
2023-11-01 15:47:44 -0300

    fix main page

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,20 +1,18 @@
 export function MainPage() {
   return (
-    <main className="-mt-3 flex h-min flex-col items-center gap-2 px-5">
+    <section className="-mt-3 flex h-min flex-col items-center gap-2 px-5">
       {/* <img class="photo" src="https://picsum.photos/200" /> */}
-      <div className=" flex w-auto max-w-4xl flex-col gap-2 md:w-full">
-        <span className="ml-1 text-sm font-medium ">Bem vindo, eu sou</span>
+      <div className="flex w-auto max-w-5xl flex-col gap-2 text-center">
+        <h3 className="text-3xl font-bold text-rose-800">Lucas Emanuel.</h3>
 
-        <h3 className="text-3xl font-bold md:text-5xl">Lucas Emanuel.</h3>
+        <h1 className="whitespace-break-spaces text-3xl font-bold tracking-tight md:text-7xl md:leading-tight">
+          Desenvolvedor Frontend {'\n'}& Mobile
+        </h1>
 
-        <h3 className="text-3xl font-bold text-rose-800 md:text-5xl">
-          Desenvolvedor frontend
-        </h3>
-
-        <span className="max-w-lg text-sm font-medium text-neutral-600">
+        <span className="text-base font-medium text-neutral-600">
           Sou um desenvolver frontend atualmente morando no Brasil.
         </span>
       </div>
-    </main>
+    </section>
   );
 }
