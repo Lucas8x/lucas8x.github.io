92d8f2a0cbd4bf27c2c3bfc9c5576e97a2c8b7a0
Lucas8x <lucasemanuel8x@gmail.com>
2023-11-03 14:34:51 -0300

    Update main.tsx

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -4,7 +4,7 @@ export function MainPage() {
   return (
     <section className="-mt-3 flex h-min flex-col items-center gap-2 px-5">
       <div className="flex w-auto max-w-5xl flex-col gap-2 text-center">
-        <h3 className=" text-2xl font-bold text-rose-800 md:text-3xl">
+        <h3 className="text-2xl font-bold text-rose-800 md:text-3xl">
           Lucas Emanuel.
         </h3>
 
@@ -16,9 +16,13 @@ export function MainPage() {
           Sou um desenvolver frontend atualmente morando no Brasil.
         </span>
 
-        <div className="mt-8 grid w-full grid-cols-4 justify-items-center gap-y-4 md:flex md:justify-between md:gap-0">
+        <div className="mt-8 grid w-full grid-cols-4 justify-items-center gap-y-4 lg:flex lg:justify-between lg:gap-0">
           {TECH_ICONS.map((i) => (
-            <img key={i} src={`https://skillicons.dev/icons?i=${i}`} alt="" />
+            <img
+              key={i}
+              src={`https://skillicons.dev/icons?i=${i}`}
+              alt={TECH_ICONS.join(', ')}
+            />
           ))}
         </div>
       </div>
